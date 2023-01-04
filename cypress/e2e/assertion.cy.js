9bf62b866c0189254d4203a1ed883fd64f2e8f50
shubhankar <smajumdar.itn@gmail.com>
2023-01-04 00:19:55 -0500

    completed assertion

--- cypress/e2e/assertion.cy.js
+++ cypress/e2e/assertion.cy.js
@@ -1,4 +1,4 @@
-// There are two types od assertions:
+// There are two types of assertions:
 
 // Implicit Assertion:
 // - should
@@ -8,20 +8,20 @@
 // - expect 
 // - assert 
 
-describe('Verify the assertion', ()=>{
-    it('should verify the implicit assertion', ()=>{
-        const url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+describe('Verify the assertion', () => {
+    const url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    it('should verify the implicit assertion', () => {
         cy.visit(url)
         //Method 01
         // cy.url().should('eq', url)
         // cy.url().should('include', 'orangehrmlive')
         // cy.url().should('contain', 'orangehrmlive.com')
-        
+
         //Method 02
         // cy.url().should('eq', url)
         // .should('include', 'orangehrmlive')
         // .should('contain', 'orangehrmlive.com')
-        
+
         //Method 03
         // cy.url().should('eq', url)
         // .and('include', 'orangehrmlive')
@@ -41,11 +41,31 @@ describe('Verify the assertion', ()=>{
 
         //Verify the inputted value right or wrong
         cy.get("input[placeholder='Username']").type("Admin")
-        .and("have.value", "Admin")
-        .and("not.have.value", "admin")
+            .and("have.value", "Admin")
+            .and("not.have.value", "admin")
 
         cy.get("input[placeholder='Password']").type("admin123")
-        .and("have.value", "admin123")
+            .and("have.value", "admin123")
+
+    })
+    it.only("Explicit Assertions", () => {
+        cy.visit(url)
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
+        cy.get("button[type='submit']").click()
+
+        let expectedName = "Paul Collings"
+        let unexpectedName = "Logan Paul"
+        cy.get('.oxd-userdropdown-tab').then((name) => {
+            let actualName = name.text()
+            //BDD Style === expect
+            expect(actualName).to.equal(expectedName)
+            expect(actualName).to.not.equal(unexpectedName)
+
+            //TDD Style ===assert
+            assert.equal(actualName, expectedName)
+            assert.notEqual(actualName, unexpectedName)
+        })
 
     })
 })
